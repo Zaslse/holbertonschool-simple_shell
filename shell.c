f6b486cd6cf2b7df76ea5c65a2f915c744cf6a9a
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-08-25 18:52:44 +0300

    Handle PATH commands

--- shell.c
+++ shell.c
@@ -7,6 +7,50 @@
 extern char **environ;
 
 /**
+ * find_command - Finds a command in PATH
+ * @command: Command to find
+ *
+ * Return: Full path or NULL
+ */
+char *find_command(char *command)
+{
+	char *path = NULL, *copy, *dir, *full;
+	int i;
+
+	if (strchr(command, '/') != NULL)
+		return (access(command, X_OK) == 0 ? strdup(command) : NULL);
+
+	for (i = 0; environ[i] != NULL; i++)
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+			path = environ[i] + 5;
+
+	if (path == NULL)
+		return (NULL);
+
+	copy = strdup(path);
+	dir = strtok(copy, ":");
+	while (dir != NULL)
+	{
+		full = malloc(strlen(dir) + strlen(command) + 2);
+		if (full == NULL)
+		{
+			free(copy);
+			return (NULL);
+		}
+		sprintf(full, "%s/%s", dir, command);
+		if (access(full, X_OK) == 0)
+		{
+			free(copy);
+			return (full);
+		}
+		free(full);
+		dir = strtok(NULL, ":");
+	}
+	free(copy);
+	return (NULL);
+}
+
+/**
  * main - Simple UNIX command interpreter
  * @argc: Argument count
  * @argv: Argument vector
@@ -15,11 +59,9 @@ extern char **environ;
  */
 int main(int argc, char **argv)
 {
-	char *line = NULL;
-	char **args;
-	char *token;
+	char *line = NULL, **args, *token, *command;
 	size_t len = 0;
-	int index;
+	int index, count = 0;
 	pid_t pid;
 
 	(void)argc;
@@ -55,16 +97,24 @@ int main(int argc, char **argv)
 			continue;
 		}
 
+		count++;
+		command = find_command(args[0]);
+		if (command == NULL)
+		{
+			fprintf(stderr, "%s: %d: %s: not found\n",
+				argv[0], count, args[0]);
+			free(args);
+			continue;
+		}
+
 		pid = fork();
 		if (pid == 0)
 		{
-			execve(args[0], args, environ);
-			perror(argv[0]);
-			free(args);
-			free(line);
+			execve(command, args, environ);
 			_exit(1);
 		}
 		wait(NULL);
+		free(command);
 		free(args);
 	}
 }
