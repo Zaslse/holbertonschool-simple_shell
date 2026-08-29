a3c9d66721b2f105f9b59c86140c1b1c8410e21e
Zaslse <zaslse@hotmail.com>
2026-08-30 02:23:21 +0300

    Add main function to print message
    Implement main function for simple shell project.

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for the simple shell project
+ *
+ * Description: Prints a message to pass Betty checks
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Betty would be proud\n");
+
+	return (0);
+}
