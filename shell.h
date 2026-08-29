67c2444274a6cc4cb505c266d0ee7137b53235b0
Zaslse <zaslse@hotmail.com>
2026-08-30 02:38:07 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -5,5 +5,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+void execute_cmd(char **argv, char *line);
 
 #endif /* SHELL_H */
