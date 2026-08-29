b39c3d7eefcfec1fc98a09598a45dbaf79e8de81
Zaslse <zaslse@hotmail.com>
2026-08-30 02:39:59 +0300

    Refactor shell.c to improve command execution

--- shell.c
+++ shell.c
@@ -1,120 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-
-extern char **environ;
+#include "shell.h"
 
 /**
- * find_command - Finds a command in PATH
- * @command: Command to find
- *
- * Return: Full path or NULL
+ * execute_cmd - Executes the command using fork and execve
+ * @argv: Array of parsed arguments
+ * @line: The original input string
  */
-char *find_command(char *command)
+void execute_cmd(char **argv, char *line)
 {
-	char *path = NULL, *copy, *dir, *full;
-	int i;
+	pid_t child_pid;
+	int status;
 
-	if (strchr(command, '/') != NULL)
-		return (access(command, X_OK) == 0 ? strdup(command) : NULL);
-
-	for (i = 0; environ[i] != NULL; i++)
-		if (strncmp(environ[i], "PATH=", 5) == 0)
-			path = environ[i] + 5;
-
-	if (path == NULL)
-		return (NULL);
-
-	copy = strdup(path);
-	dir = strtok(copy, ":");
-	while (dir != NULL)
+	child_pid = fork();
+	if (child_pid == 0)
 	{
-		full = malloc(strlen(dir) + strlen(command) + 2);
-		if (full == NULL)
+		if (execve(argv[0], argv, environ) == -1)
 		{
-			free(copy);
-			return (NULL);
+			perror("./hsh");
+			free(line);
+			exit(1);
 		}
-		sprintf(full, "%s/%s", dir, command);
-		if (access(full, X_OK) == 0)
-		{
-			free(copy);
-			return (full);
-		}
-		free(full);
-		dir = strtok(NULL, ":");
 	}
-	free(copy);
-	return (NULL);
+	else
+	{
+		wait(&status);
+	}
 }
 
 /**
- * main - Simple UNIX command interpreter
- * @argc: Argument count
- * @argv: Argument vector
+ * main - Simple shell entry point
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
-int main(int argc, char **argv)
+int main(void)
 {
-	char *line = NULL, **args, *token, *command;
+	char *line = NULL, *token, *argv[100];
 	size_t len = 0;
-	int index, count = 0;
-	pid_t pid;
+	int i;
 
-	(void)argc;
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-			printf("#cisfun$ ");
+			printf("($) ");
 		if (getline(&line, &len, stdin) == -1)
 		{
 			free(line);
-			return (0);
+			if (isatty(STDIN_FILENO))
+				printf("\n");
+			exit(0);
 		}
-
-		args = malloc(sizeof(char *) * len);
-		if (args == NULL)
+		token = strtok(line, " \n");
+		if (token == NULL)
+			continue;
+		if (strcmp(token, "exit") == 0)
 		{
 			free(line);
-			return (1);
+			exit(0);
 		}
-
-		index = 0;
-		token = strtok(line, " \n");
-		while (token != NULL)
+		for (i = 0; token != NULL; i++)
 		{
-			args[index++] = token;
+			argv[i] = token;
 			token = strtok(NULL, " \n");
 		}
-		args[index] = NULL;
-
-		if (args[0] == NULL)
-		{
-			free(args);
-			continue;
-		}
-
-		count++;
-		command = find_command(args[0]);
-		if (command == NULL)
-		{
-			fprintf(stderr, "%s: %d: %s: not found\n",
-				argv[0], count, args[0]);
-			free(args);
-			continue;
-		}
-
-		pid = fork();
-		if (pid == 0)
-		{
-			execve(command, args, environ);
-			_exit(1);
-		}
-		wait(NULL);
-		free(command);
-		free(args);
+		argv[i] = NULL;
+		execute_cmd(argv, line);
 	}
+	return (0);
 }
