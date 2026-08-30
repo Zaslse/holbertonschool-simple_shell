0190d900644133c679c64941e1bfdc3ba9006d13
zaslse <zaslse@hotmail.com>
2026-08-30 22:23:30 +0000

    Fix _getline memory bugs and correct cd dash missing OLDPWD output

--- builtins2.c
+++ builtins2.c
@@ -19,11 +19,9 @@ int builtin_cd(info_t *info)
 	}
 	else if (_strcmp(info->args[1], "-") == 0)
 	{
-		if (oldpwd == NULL)
-			target = pwd;
-		else
+		target = oldpwd ? oldpwd : pwd;
+		if (target != NULL)
 		{
-			target = oldpwd;
 			write(STDOUT_FILENO, target, _strlen(target));
 			write(STDOUT_FILENO, "\n", 1);
 		}
@@ -31,17 +29,26 @@ int builtin_cd(info_t *info)
 	else
 		target = info->args[1];
 
+	if (target == NULL)
+		target = "/";
+
 	if (chdir(target) == -1)
 	{
 		print_custom_error(info, "cd", "can't cd to ");
-		write(STDERR_FILENO, target, _strlen(target));
+		write(STDERR_FILENO, info->args[1], _strlen(info->args[1]));
 		write(STDERR_FILENO, "\n", 1);
 		info->status = 2;
 		return (1);
 	}
-	_setenv(info, "OLDPWD", pwd ? pwd : "");
+
+	if (pwd != NULL)
+		_setenv(info, "OLDPWD", pwd);
+	else if (getcwd(cwd, sizeof(cwd)) != NULL)
+		_setenv(info, "OLDPWD", cwd);
+
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
 		_setenv(info, "PWD", cwd);
+
 	info->status = 0;
 	return (1);
 }
--- getline.c
+++ getline.c
@@ -10,48 +10,56 @@
 ssize_t _getline(char **lineptr, size_t *n, int fd)
 {
 	static char buffer[BUFSIZE];
-	static ssize_t b_pos;
-	static ssize_t b_size;
-	ssize_t len = 0;
+	static ssize_t b_pos = 0;
+	static ssize_t b_size = 0;
+	ssize_t len = 0, i;
 	char *new_buf;
 
 	if (lineptr == NULL || n == NULL)
 		return (-1);
+
 	if (*lineptr == NULL || *n == 0)
 	{
 		*n = BUFSIZE;
 		*lineptr = malloc(*n);
 		if (*lineptr == NULL)
 			return (-1);
+		(*lineptr)[0] = '\0';
 	}
+
 	while (1)
 	{
 		if (b_pos >= b_size)
 		{
 			b_size = read(fd, buffer, BUFSIZE);
 			b_pos = 0;
-			if (b_size <= 0)
+			if (b_size == 0)
 			{
 				if (len == 0)
 					return (-1);
 				(*lineptr)[len] = '\0';
 				return (len);
 			}
+			if (b_size < 0)
+				return (-1);
 		}
+
 		while (b_pos < b_size)
 		{
-			if (len + 2 >= (ssize_t)*n)
+			if (len >= (ssize_t)(*n) - 1)
 			{
 				*n += BUFSIZE;
 				new_buf = malloc(*n);
 				if (new_buf == NULL)
 					return (-1);
-				_strcpy(new_buf, *lineptr);
+				for (i = 0; i < len; i++)
+					new_buf[i] = (*lineptr)[i];
 				free(*lineptr);
 				*lineptr = new_buf;
 			}
-			(*lineptr)[len++] = buffer[b_pos];
-			if (buffer[b_pos++] == '\n')
+			(*lineptr)[len] = buffer[b_pos++];
+			len++;
+			if (buffer[b_pos - 1] == '\n')
 			{
 				(*lineptr)[len] = '\0';
 				return (len);
